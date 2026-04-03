fea494148abaec41d24d38ac297bf1c969faba64
gabrielwork4-hub <seu@email.com>
2026-04-03 19:01:17 -0300

    chore: upload massivo de webps otimizados e finalização completa da estrutura seo dos carrosseis

--- script.js
+++ script.js
@@ -2,32 +2,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const imageLists = {
         'bolos': [
+            "bolo-80-anos-marina.webp",
+            "bolo-9-meses-poderosaliz.webp",
+            "bolo-aniversario-1-ano-urso-castelo.webp",
+            "bolo-aniversario-10-anos-casamento-rosas-vermelhas.webp",
             "bolo-aniversario-21-anos-mulher-rosa-borboletas-estrelas.webp",
             "bolo-aniversario-divertidamente-6-meses-beatriz.webp",
             "bolo-aniversario-florido-borboletas-lusinete-62-anos.webp",
+            "bolo-aniversario-homem-aranha.webp",
+            "bolo-aniversario-moana-3-anos-laura-jpg.webp",
             "bolo-aniversario-peppapig-personagens-marinhos.webp",
             "bolo-aniversario-rei-trono-ha-jonatas-32-anos.webp",
             "bolo-aniversario-stitch-lorenzo-10anos.webp",
+            "bolo-aniversario-super-mario-heroico.webp",
+            "bolo-casa-rosa-clinica-estetica-avancada-jpg.webp",
+            "bolo-de-aniversario-henrique-5-anos-bob-esponja.webp",
             "bolo-de-aniversario-infantil-tema-fazenda-para-beatryz.webp",
             "bolo-de-aniversario-para-bebe-anya-com-tema-de-ovelhinhas-e-nuvens-png.webp",
+            "bolo-de-aniversario-rosa-46-anos.webp",
+            "bolo-de-cor-de-floresta-encantada-unicornio-3-meses-beatuz.webp",
+            "bolo-de-crisma-com-igreja-e-oracao.webp",
             "bolo-de-quase-um-ano-bebe-tema-advogado-jpg.webp",
             "torta-aniversario-infantil-tema-de-aviao-ursinho-1-mes.webp"
         ],
         'salgados': [
-            "Imagem do WhatsApp de 2024-11-01 à(s) 17.36.44_57f8d39a.jpg",
-            "Imagem do WhatsApp de 2024-11-01 à(s) 17.36.46_65068fe3.jpg",
-            "Imagem do WhatsApp de 2024-11-01 à(s) 17.36.48_6246192e.jpg",
-            "Imagem do WhatsApp de 2024-11-01 à(s) 17.37.00_484fb58e.jpg",
-            "Imagem do WhatsApp de 2024-11-01 à(s) 17.37.00_7c067f82.jpg",
-            "Imagem do WhatsApp de 2024-11-01 à(s) 17.37.06_e0ad9292.jpg",
-            "Imagem do WhatsApp de 2024-11-01 à(s) 17.37.09_2c7863d6.jpg"
+            "efiha-de-escarola-em-preparacao.webp",
+            "empadas-de-frango-com-catupiry.webp",
+            "esfiha-de-carne-caseira.webp",
+            "esfiha-de-escarola-caseira.webp",
+            "esfiha-de-frango-caseira.webp",
+            "risoles-de-presunto-e-queijo.webp",
+            "salgadinhos-de-festa-varios-tipos-emcaixas-para-venda.webpsalgadinhos-de-festa-varios-tipos-em-caixas-para-venda.webp"
         ],
         'doces': [
+            "brigadeiros-e-bixos-de-pe.webp",
+            "brigadeiros-e-docinhos-de-morango.webp",
             "brigadeiros-tradicionais-morango-e-chocolate.webp",
             "docinho-de-leite-ninho-receita-nutritiva.webp",
             "docinho-de-leite-ninho.webp",
             "docinhos-cajuzinhos-e-beijinhos.webp",
+            "docinhos-de-coco-beijinho.webp",
             "docinhos-festa-coloridos-variados.webp",
+            "docinhos-sortidos-gourmet.webp",
             "docinhos-variados-bichos-de-pé-cajuzinho-beijinho.webp",
             "variedade-de-brigadeiros-tradicionais-comestiveis-de-colher.webp"
         ]
